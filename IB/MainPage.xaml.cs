a33e277e014cbdfcfaf110498e094eaeecc8f9bd
Barengific <75044077+Barengific@users.noreply.github.com>
2023-02-21 10:46:15 -0700

    Update MainPage.xaml.cs

--- IB/MainPage.xaml.cs
+++ IB/MainPage.xaml.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+
+
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Documents;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using Paragraph = iText.Layout.Element.Paragraph;
 
 namespace IB
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+
+            // Must have write permissions to the path folder
+            //PdfWriter writer = new PdfWriter("C:\\Users\\Admin\\Desktop\\testt\\demo.pdf");
+            //PdfDocument pdf = new PdfDocument(writer);
+            //Document document = new Document(pdf);
+            //Paragraph header = new Paragraph("HEADER")
+            //   .SetTextAlignment(TextAlignment.CENTER)
+            //   .SetFontSize(20);
+
+            //document.Add(header);
+            //document.Close();
+
         }
+
     }
 }
