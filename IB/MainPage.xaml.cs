9d36e2fac929a26f07f68e30bcdeee060ba7ac4e
Barengific <75044077+Barengific@users.noreply.github.com>
2023-02-26 10:49:34 -0800

    Update MainPage.xaml.cs

--- IB/MainPage.xaml.cs
+++ IB/MainPage.xaml.cs
@@ -24,6 +24,9 @@ using Paragraph = iText.Layout.Element.Paragraph;
 
 using Microsoft.Data.Sqlite;
 
+using System.Security.Permissions;
+using System.Security;
+
 namespace IB
 {
 
@@ -34,37 +37,75 @@ namespace IB
             this.InitializeComponent();
 
             // Must have write permissions to the path folder
-            //PdfWriter writer = new PdfWriter("C:\\Users\\Admin\\Desktop\\testt\\demo.pdf");
+            //PdfWriter writer = new PdfWriter("D:\\Admino\\demo.pdf");
             //PdfDocument pdf = new PdfDocument(writer);
             //Document document = new Document(pdf);
             //Paragraph header = new Paragraph("HEADER")
-            //   .SetTextAlignment(TextAlignment.CENTER)
             //   .SetFontSize(20);
 
             //document.Add(header);
             //document.Close();
 
+            //LvUsers.Items.Add(new UsersList()
+            //{
+            //    PersonID = getUsers?.Response?.Data?.PersonList?.PersonInfoList?[i]?.PersonID.ToString(),
+            //    PersonName = getUsers?.Response?.Data?.PersonList?.PersonInfoList?[i]?.PersonName,
+            //    Bday = getUsers?.Response?.Data?.PersonList?.PersonInfoList?[i]?.Birthday,
+            //    Gender = getUsers?.Response?.Data?.PersonList?.PersonInfoList?[i]?.Gender.ToString(),
+            //    EmployeeID = getUsers?.Response?.Data?.PersonList?.PersonInfoList?[i]?.IdentificationList?[0]?.Number
+            //});
 
+            ItemsLists.Items.Add(new ItemssList() {
+                ItemDescription ="aa", 
+                ItemCost = "bb",
+                ItemVatPer = "ccc",
+                ItemVATCost = "ddd"
+                });
+
+                    
+
+        ItemsLists.Items.Add(new ItemssList()
+        {
+            ItemDescription = "zz", 
+                ItemCost = "xxx",
+                ItemVatPer = "yy",
+                ItemVATCost = "uu"
+                });
+
+            TermsList.Items.Add(new TermsList()
+            {
+                TermNumber = "aaa",
+                TermDescription = "bbbbbbbbbbbbbbbb"
+            });
+
+            TermsList.Items.Add(new TermsList()
+            {
+                TermNumber = "tttt",
+                TermDescription = "rrrrrr"
+            });
 
         }
-
-        private void BtnAddItem(object sender, RoutedEventArgs e)
+    private void BtnAddItem(object sender, RoutedEventArgs e)
         {
 
         }
-
         private void BtnAddTerms(object sender, RoutedEventArgs e)
         {
 
         }
     }
-
-    public class ItemList
+    public class ItemssList
     {
         public string ItemDescription { get; set; }
-        public double ItemCost { get; set; }
-        public double ItemVatPer { get; set; }
-        public double ItemVATCost { get; set; }
+        public string ItemCost { get; set; }
+        public string ItemVatPer { get; set; }
+        public string ItemVATCost { get; set; }
+    }
+
+    public class TermsList
+    {
+        public string TermNumber { get; set; }
+        public string TermDescription { get; set; }
     }
 
 
