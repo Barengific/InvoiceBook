f0badd892a71bb97791ccd9bcd729121d3cfc6ed
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-10 11:01:59 +0000

    Create HelloController.java

---
+++ IBJ/src/main/java/com/barengific/ibj/HelloController.java
@@ -0,0 +1,21 @@
+package com.barengific.ibj;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private TextField txtDescription;
+
+    @FXML
+    protected void onAddItemBtn() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onAddTermsBtn() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
