82dab1882d7704a345804f4c773f264151841e83
Barengific <75044077+Barengific@users.noreply.github.com>
2023-03-12 19:26:05 +0000

    Update module-info.java

--- IBJ/src/main/java/module-info.java
+++ IBJ/src/main/java/module-info.java
@@ -2,6 +2,11 @@ module com.barengific.ibj {
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
+    requires itextpdf;
+
+//    requires com.itextpdf;
+//
+//    requires org.apache.pdfbox;
 
     opens com.barengific.ibj to javafx.fxml;
     exports com.barengific.ibj;
